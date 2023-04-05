aae6263122099d57192a32f30464e6a51f4b9382
AhmedNagm <ahmed-nagm@outlook.com>
2023-04-05 03:53:44 +0000

    locked class error fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Define a locked class"""
+"""Defines a class LockedClass"""
 
 
 class LockedClass:
+    """
+    Prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name.
+    Attributes:
+        first_name (str): first name of something.
+    """
+
     __slots__ = ["first_name"]
 
     def __init__(self):
-        pass
+        """Creates new instances of Locked Class."""
 
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            raise AttributeError(
-                f"{name} is not an allowed attribute for this class")
-        super().__setattr__(name, value)
+        self.first_name = "first_name"
