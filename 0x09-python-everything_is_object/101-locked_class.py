37702ee45ca5eef18d72378d0228d8683233a6c6
AhmedNagm <ahmed-nagm@outlook.com>
2023-04-05 03:30:31 +0000

    locked class fix and implemet slot

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
+"""Define a locked class"""
+
+
 class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass 
+    for anything but attributes called 'first_name'.
+    """
 
-    __slots__ = "first_name"
-
-    def __init__(self, first_name=None):
-        self. first_name = first_name
+    __slots__ = ["first_name"]
